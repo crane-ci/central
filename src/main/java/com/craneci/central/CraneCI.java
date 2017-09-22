c482bfc86cbc5da0988a505c0663041ac545e1b0
ariefwara <arief.wara@gmail.com>
2017-09-22 10:18:45 +0700

    initial commit

---
+++ src/main/java/com/craneci/central/CraneCI.java
@@ -0,0 +1,13 @@
+package com.craneci.central;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraneCI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraneCI.class, args);
+	}
+	
+}
---
+++ src/test/java/com/craneci/central/CraneCITests.java
@@ -0,0 +1,16 @@
+package com.craneci.central;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CraneCITests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
